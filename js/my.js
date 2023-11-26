b126bc610e49ff5bc8125256061cbbc6f587d4e1
Sonya <harinova@mail.ru>
2023-11-26 18:36:21 +0300

    initial commit

--- js/my.js
+++ js/my.js
@@ -1,18 +1,13 @@
-/*
-function changeHeadingColor() {
-    const heading = document.querySelector(".heading");
-    const scroll = window.pageYOffset || document.documentElement.scrollTop;
-    const screenSize = window.innerWidth || document.documentElement.clientWidth;
-  
-    if (scroll > 800 && screenSize > 768) {
-        heading.style.background = "rgba(20, 17, 17, 0.60)";
-    } else if (scroll >= 400 && screenSize <= 768) {
-        heading.style.background = "rgba(20, 17, 17, 0.60)";
-    } else {
-        heading.style.background = "rgba(243, 239, 232, 0.60)";
-    }
-  }
 
+function changeHeadingColor() {
+    let scrollHeight = window.scrollY;   
+    let heading = document.querySelector('.heading');    
+  
+    if (scrollHeight > window.innerHeight) {  
+      heading.style.background = "rgba(20, 17, 17, 0.60)";
+    } else {
+      heading.style.background = 'rgba(243, 239, 232, 0.60)';
+    }
+}
   window.addEventListener('scroll', changeHeadingColor);
-  window.addEventListener('resize', changeHeadingColor);
-*/
+
