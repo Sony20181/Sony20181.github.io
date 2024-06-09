e06c55cd1b901a2ca77c9c5c1c183bfe5f2a6ee9
Sony20181 <harinova2003@mail.ru>
2024-06-09 18:59:19 +0300

    доделать форму на 1024 и 360 и дальше

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ function changeHeadingColor() {
     let scrollHeight = window.scrollY;
     let heading = document.querySelector('.heading');
     if (scrollHeight < 450) {
-      heading.style.background = null;
+      heading.style.backgroundColor = null;
+      heading.style.backdropFilter = null;
       heading.style.position = "absolute";
       } 
     else {
-      heading.style.background = "rgba( 243, 239, 232, 0.6)";
-      heading.style.webkitFilter = "blur(54px);";
+      heading.style.backgroundColor = "rgba(243, 239, 232, 0.6)";
+      heading.style.backdropFilter = "blur(54px)";
       heading.style.position = "fixed";
       heading.style.marhinBottom = "88px";
     }
